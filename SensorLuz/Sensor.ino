81582493e61132e9dc15f6f23a5be2b84995951a
TI-Lorca <103193452+TI-Lorca@users.noreply.github.com>
2022-05-13 14:31:20 -0300

    Create Sensor.ino

---
+++ SensorLuz/Sensor.ino
@@ -0,0 +1,19 @@
+int sensorReading;
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(6,OUTPUT); // 
+}
+
+void loop() {
+  
+ sensorReading=analogRead(0);
+  
+ if (sensorReading<700)
+ {
+ digitalWrite(6,HIGH);
+ }
+ else digitalWrite(6,LOW);
+ Serial.println(sensorReading); 
+ delay(1000);
+}
